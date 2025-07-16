a89bfdf4ba55642505934ffc24ffec420605efea
Gerson Manuel Sugianto <dev.gersonmanuel@gmail.com>
2025-07-17 04:24:00 +0700

    Update login redirect route to point to user login

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/formdonasi', [HomeController::class, 'formdonasi'])->name('formdonasi');
 });
 
-Route::redirect('/login', '/admin/login')->name('login');
+Route::redirect('/login', '/santun/login')->name('login');
 
 
 Route::get('/auth/{provider}/redirect', [SocialiteController::class, 'redirect'])
