2ce186e3fedd2be4a5e48b919ee8fecabd2e8d49
Gerson Manuel Sugianto <dev.gersonmanuel@gmail.com>
2025-07-17 05:03:30 +0700

    Define table names explicitly for Item and Money models

--- app/Models/Money.php
+++ app/Models/Money.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Money extends Model
 {
+    protected $table = 'moneys'; // Explicitly tell Eloquent
     protected $fillable = [
         'donasi_id',
         'total',
