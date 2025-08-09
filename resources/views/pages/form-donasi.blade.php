a506f3e78d09fb304c2d7b88d996d15d6d2c284e
Gerson Manuel Sugianto <dev.gersonmanuel@gmail.com>
2025-08-10 02:09:23 +0700

    3xl

--- resources/views/pages/form-donasi.blade.php
+++ resources/views/pages/form-donasi.blade.php
@@ -105,7 +105,7 @@
     </div>
 
     <div class="bg-white py-8">
-        <div class="mx-auto max-w-2xl p-4 md:p-8 rounded-xl shadow-xl/30">
+        <div class="mx-auto max-w-3xl p-4 md:p-8 rounded-xl shadow-xl/30">
             <h2 class="text-2xl font-bold text-gray-800 mb-6 text-center">Form Donasi</h2>
 
             <form action="{{ route('formdonasi.submit') }}" method="POST" enctype="multipart/form-data"
