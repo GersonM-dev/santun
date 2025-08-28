a7efe7691e6ba4737f055ae238f57e183b2e73f6
GersonM-dev <dev.gersonmanuel@gmail.com>
2025-08-29 03:00:34 +0700

    Refactor code structure for improved readability and maintainability

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,10 +2,7 @@
     class="mb-8 mt-2 flex items-center justify-between py-2 md:mb-12 md:py-4 xl:mb-16 shadow-xl/30 p-6 rounded-xl bg-white">
     <!-- logo - start -->
     <a href="/" class="inline-flex items-center gap-2.5 text-2xl font-bold text-black md:text-3xl" aria-label="logo">
-        <svg width="95" height="94" viewBox="0 0 95 94" class="h-auto w-6 text-sky-500" fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg">
-            <path d="M96 0V47L48 94H0V47L48 0H96Z" />
-        </svg>
+        <img src="{{ asset('logo.PNG') }}" alt="Logo" class="h-auto w-6">
         Santun
     </a>
     <!-- logo - end -->
