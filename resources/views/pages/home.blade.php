d4ee31bb1c521700c573f688ce14b97d4e78c12e
GersonM-dev <dev.gersonmanuel@gmail.com>
2025-08-29 03:14:14 +0700

    Update hero section heading for improved readability and consistency

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -6,7 +6,7 @@
     <section class="flex flex-col justify-between gap-6 sm:gap-10 md:gap-16 lg:flex-row">
         <!-- content - start -->
         <div class="flex flex-col justify-center sm:text-center lg:py-12 lg:text-left xl:w-5/12 xl:py-24">
-            <h1 class="mb-8 text-3xl font-bold text-black sm:text-4xl md:mb-12 md:text-5xl">
+            <h1 class="mb-8 text-xl font-bold text-black sm:text-xl md:mb-12 md:text-3xl">
                 Mari bersama berbagi kebaikan, saling mendukung, dan memberi harapan baru lewat Relawan ODGJ Baturraden</h1>
             <p class="mb-8 leading-relaxed text-gray-500 md:mb-12 lg:w-4/5 xl:text-lg">
                 Dengan semangat kepedulian, Relawan ODGJ Baturraden berkomitmen untuk memberikan layanan sosial,
