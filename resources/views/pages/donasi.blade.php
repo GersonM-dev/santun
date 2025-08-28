a34e401da6771d973d12446520e5183c5cb6416a
GersonM-dev <dev.gersonmanuel@gmail.com>
2025-08-29 03:50:02 +0700

    Update image display properties for better visual consistency

--- resources/views/pages/donasi.blade.php
+++ resources/views/pages/donasi.blade.php
@@ -18,7 +18,7 @@
                 <a href="{{ route('formdonasi', 'materi') }}" class="group relative flex h-80 items-end overflow-hidden rounded-lg bg-gray-100 p-4 shadow-lg">
                     <img src="{{ asset('donasi materi.PNG') }}"
                         loading="lazy" alt="Photo by Fakurian Design"
-                        class="absolute inset-0 h-full w-full object-contain object-center transition duration-200 group-hover:scale-110" />
+                        class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                     <div
                         class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
@@ -34,7 +34,7 @@
                 <a href="{{ route('formdonasi' , 'non-materi') }}" class="group relative flex h-80 items-end overflow-hidden rounded-lg bg-gray-100 p-4 shadow-lg">
                     <img src="{{ asset('donasi non-materi.PNG') }}"
                         loading="lazy" alt="Photo by Fakurian Design"
-                        class="absolute inset-0 h-full w-full object-contain object-center transition duration-200 group-hover:scale-110" />
+                        class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
 
                     <div
                         class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
