9cf18d0d288abf962cf6f19358d16107f80bf4db
GersonM-dev <dev.gersonmanuel@gmail.com>
2025-09-19 17:01:08 +0700

    Add labels to create actions in ListBantuans and ListDonasis pages

--- app/Filament/Resources/DonasiResource/Pages/ListDonasis.php
+++ app/Filament/Resources/DonasiResource/Pages/ListDonasis.php
@@ -13,7 +13,7 @@ class ListDonasis extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()->label('Tambah Donasi'),
         ];
     }
 }
